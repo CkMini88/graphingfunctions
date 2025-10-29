f9bfc003f0104d27e0bbc880d481feac666b9393
[dyad] <git@dyad.sh>
2025-10-29 16:23:33 +0300

    [dyad] Fixing plotly.js buffer error - wrote 3 file(s), added buffer, @esbuild-plugins/node-globals-polyfill, @esbuild-plugins/node-modules-polyfill package(s)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 export default defineConfig(() => ({
   server: {
@@ -12,6 +14,21 @@ export default defineConfig(() => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global / module polyfill options
+      define: {
+        global: 'globalThis', // Define global for older libraries
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
     },
   },
 }));
